b442c082dc60886017436e6fa3520bb9f6fa3e1a
jlacko <jindra.lacko@gmail.com>
2017-09-02 16:37:30 +0000

    including report

--- CenovaMapa.R
+++ CenovaMapa.R
@@ -50,23 +50,29 @@ frmBenzinKey <- frmBenzin %>%
 
 obce_body$key <- paste(obce_body$Obec, obce_body$Okres, sep = "/")
 
-obce_body <- append_data(obce_body, frmBenzinKey, key.shp = "key", key.data = "key", ignore.na = T) # ignoruju NA = zahazuju nespárované obce (neznámá cena beňa)
-obce_body <- subset(obce_body, !is.na(obce_body$cena)) # zahazuju obce s neznámou cenou benzínu
+vObce <- c("Praha", "Brno", "Plzeň", "Ostrava") # velké obce - nejsou puntíkem, ale obrysem
 
+obce_body <- obce_body %>%
+  append_data(frmBenzinKey, key.shp = "key", key.data = "key", ignore.na = T) %>% # ignoruju NA = zahazuju nespárované obce (neznámá cena beňa)
+  subset(!is.na(obce_body$cena)) %>%  # zahazuju obce s neznámou cenou benzínu
+  subset(!obce_body$Obec %in% vObce) # ať se netluče puntík s polygonem
+
+wrkObce <- obce_polygony[obce_polygony$Obec %in% vObce, ] 
+  
 # vlastí kreslení... ----
 
 nadpis <- "Cena benzínu po obcích v České republice" # nadpis grafu
 leyenda <- "Cena Naturalu 95"  # nadpis legendy
 endCredits <- paste("zdroj dat: Ráádio Impuls (http://benzin.impuls.cz), staženo k", format(max(frmBenzin$datum), "%d.%m.%Y") ,sep = " ")
 
-wrkObce <- obce_polygony[obce_polygony$Obyvatel > 300000, ] # Praha, Brno
-
-plot <-   tm_shape(obce_body, bbox = bbox) + tm_bubbles(size = 1/25, col = "cena", alpha = 0.85, border.alpha = 0, showNA = F, pal = "YlOrRd", title.col = leyenda) +
+tmBenzin <-   tm_shape(obce_body, bbox = bbox) + tm_bubbles(size = 1/40, col = "cena", alpha = 0.85, border.alpha = 0, showNA = F, pal = "YlOrRd", title.col = leyenda) +
   tm_shape(republika, bbox = bbox) + tm_borders("grey30", lwd = 1) +
-  tm_shape(wrkObce) + tm_borders("grey20", lwd = 0.5)+
-  tm_style_white(nadpis, frame = F, fontfamily = "Roboto", legend.text.size = 0.5, legend.title.size = 0.7, legend.format = list(text.separator = "-", fun=function(x) paste0(formatC(x, digits=0, format="f"), " Kč")))+
-  tm_credits(endCredits, position = c("RIGHT", "BOTTOM"), size = 0.4, col = "grey35")
+  tm_shape(wrkObce) + tm_borders("grey30", lwd = 0.5)+
+  tm_style_white(nadpis, frame = F, fontfamily = "Roboto", legend.text.size = 0.4, legend.title.size = 0.7, legend.format = list(text.separator = "-", fun=function(x) paste0(formatC(x, digits=0, format="f"), " Kč")))+
+  tm_credits(endCredits, position = c("RIGHT", "BOTTOM"), size = 0.4, col = "grey30")
 
-save_tmap(plot , filename = "benzin.png", width = 1600, type = "cairo")
+# print(tmBenzin)
+
+save_tmap(tmBenzin , filename = "benzin.png", width = 1600, type = "cairo")
 
 
